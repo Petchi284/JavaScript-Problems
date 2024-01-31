b3a98daa7b503d5464b4c7e0f3e189327f7b43d0
Petchiammal k <145541034+Petchi284@users.noreply.github.com>
2024-01-31 20:19:36 +0530

    Time: 65 ms, Memory: 55.6 MB - LeetHub

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let value = s.reverse();
+    return value;
+};
+let user = ["h","e","l","l","o"]
+console.log(reverseString(user));
